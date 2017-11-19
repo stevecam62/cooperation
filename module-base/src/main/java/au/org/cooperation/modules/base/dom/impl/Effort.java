4d9b5ad9718d2e98b6533cb7d5ba431c59ca6fda
stevecam62 <steve.cameron.62@gmail.com>
2017-11-20 09:54:08 +1100

    Effort

--- module-base/src/main/java/au/org/cooperation/modules/base/dom/impl/Effort.java
+++ module-base/src/main/java/au/org/cooperation/modules/base/dom/impl/Effort.java
@@ -1,7 +1,9 @@
 package au.org.cooperation.modules.base.dom.impl;
 
 import java.util.Date;
+import java.util.List;
 
+import javax.inject.Inject;
 import javax.jdo.annotations.Column;
 import javax.jdo.annotations.IdentityType;
 import javax.jdo.annotations.PersistenceCapable;
@@ -14,25 +16,24 @@ import javax.xml.bind.annotation.XmlType;
 import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
 
 import org.apache.isis.applib.annotation.DomainObject;
+import org.apache.isis.applib.annotation.ParameterLayout;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "Effort", propOrder = {
-	    "result",
-	    "start",
-	    "end"
-	})
+@XmlType(name = "Effort", propOrder = { "result", "start", "end" })
 @PersistenceCapable(identityType = IdentityType.DATASTORE, schema = "cooperation")
 @DomainObject()
 public class Effort {
-	
+
+	@XmlElement()
 	@Column(allowsNull = "true")
 	@Getter
-	@Setter
+	@Setter(value = AccessLevel.PRIVATE)
 	private Result result;
-	
+
 	@XmlTransient
 	@Column(allowsNull = "false")
 	@Getter
@@ -45,22 +46,22 @@ public class Effort {
 	@Setter
 	private Task task;
 
-    @XmlElement(required = true, type = String.class)
-    @XmlJavaTypeAdapter(Adapter1 .class)
-    @XmlSchemaType(name = "dateTime")
+	@XmlElement(required = true, type = String.class)
+	@XmlJavaTypeAdapter(Adapter1.class)
+	@XmlSchemaType(name = "dateTime")
 	@Column(allowsNull = "true")
 	@Getter
 	@Setter
-    protected Date start;
+	protected Date start;
 
-    @XmlElement(required = true, type = String.class)
-    @XmlJavaTypeAdapter(Adapter1 .class)
-    @XmlSchemaType(name = "dateTime")
+	@XmlElement(required = true, type = String.class)
+	@XmlJavaTypeAdapter(Adapter1.class)
+	@XmlSchemaType(name = "dateTime")
 	@Column(allowsNull = "true")
 	@Getter
 	@Setter
-    protected Date end;
-	
+	protected Date end;
+
 	Effort() {
 	}
 
@@ -70,5 +71,36 @@ public class Effort {
 		setStart(start);
 		setEnd(end);
 	}
+
+	public Effort assignToResult(Result result) {
+		this.setResult(result);
+		this.getTask().getResults().add(result);
+		return this;
+	}
+	
+	public String disableAssignToResult(){
+		if(this.getTask() == null || this.getTask().getResults().size() == 0)
+			return "No parent Task or Results associated with the parent Task";
+		else
+			return null;
+	}
+	
+	public List<Result> choices0AssignToResult(){
+		if(this.getTask() != null && this.getTask().getResults().size() > 0)
+			return this.getTask().getResults();
+		else
+			return null;		
+	}
+
+	public Effort addResult(@ParameterLayout(named = "Result description") String description) {
+		Result result = taskRepository.createResult(this.getTask(), description);
+		this.setResult(result);
+		this.getTask().getResults().add(result);
+		return this;
+	}
+	
+	@XmlTransient
+	@Inject
+	TaskRepository taskRepository;
 
 }
