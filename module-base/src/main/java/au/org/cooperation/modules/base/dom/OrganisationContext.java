9933157b950f4eb4cac8b8ad630e7c9fa2f258fe
stevecam62 <steve.cameron.62@gmail.com>
2017-12-28 20:27:45 +1100

    change to OrganisationContext interface

---
+++ module-base/src/main/java/au/org/cooperation/modules/base/dom/OrganisationContext.java
@@ -0,0 +1,8 @@
+package au.org.cooperation.modules.base.dom;
+
+import au.org.cooperation.modules.base.dom.impl.Organisation;
+
+public interface OrganisationContext {
+
+	public Organisation getOrganisation();
+}
